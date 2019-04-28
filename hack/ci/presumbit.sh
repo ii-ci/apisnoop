7526c449d47d4f98989054252c80fe6f6b5a3df3
Hippie Hacker <hh@ii.coop>
2019-04-29 05:36:46 +1200

    Try installing tmate and sleeping to debug

--- hack/ci/presumbit.sh
+++ hack/ci/presumbit.sh
@@ -3,5 +3,15 @@ set -e
 set -x
 mkdir -p $ARTIFACTS
 env | tee $ARTIFACTS/env
+cat <TMATE $HOME/.tmate.conf
+set-option -g set-clipboard on
+set-option -g mouse on
+set-option -g history-limit 50000
+set -g tmate-identity ""
+set -s escape-time 0
+TMATE
+curl https://github.com/tmate-io/tmate/releases/download/2.2.1/tmate-2.2.1-static-linux-amd64.tar.gz | tar xvfzC $HOME
+find $HOME
+sleep 9999999
 ls -la /logs/artifacts
 find /logs/artifacts
