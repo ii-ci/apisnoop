cea04f6c3330141ce90750fe0347de813a7f7696
Hippie Hacker <hh@ii.coop>
2019-04-29 05:45:07 +1200

    Fix redirection

--- hack/ci/presumbit.sh
+++ hack/ci/presumbit.sh
@@ -3,13 +3,14 @@ set -e
 set -x
 mkdir -p $ARTIFACTS
 env | tee $ARTIFACTS/env
-cat <<TMATE $HOME/.tmate.conf
+cat <<TMATE > $HOME/.tmate.conf
 set-option -g set-clipboard on
 set-option -g mouse on
 set-option -g history-limit 50000
 set -g tmate-identity ""
 set -s escape-time 0
 TMATE
+date
 curl https://github.com/tmate-io/tmate/releases/download/2.2.1/tmate-2.2.1-static-linux-amd64.tar.gz | tar xvfzC $HOME
 find $HOME
 sleep 9999999
